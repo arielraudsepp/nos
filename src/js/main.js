a04a59e77872f70a90b3221ce601db060e0e2c53
Ariel Raudsepp <ariel.raudsepp@gmail.com>
2020-10-18 19:00:00 -0600

    Add styling to make it sparkle ✨

--- src/js/main.js
+++ src/js/main.js
@@ -2,50 +2,50 @@
 if (module.hot) {
   module.hot.accept()
 }
-
+let childrenShown = false;
 //logic portion for allocating bedroom size based on family composistion
 var adult_bedrooms = (adults,spouse,children) => {
-    let bedrooms = adults;
-    if (spouse) {
-        bedrooms -= 1;
-    }
-    if ((children === false) && (adults === 1)){
-        bedrooms -= 1;
-    }
-return bedrooms
+  let bedrooms = adults;
+  if (spouse) {
+    bedrooms -= 1;
+  }
+  if ((children === false) && (adults === 1)){
+    bedrooms -= 1;
+  }
+  return bedrooms
 }
 
 var children_bedrooms = (f_5_to_17, f_under_5, m_5_to_17, m_under_5) => {
-    let bedrooms = 0;
-    let females = f_5_to_17 + f_under_5;
-    let males = m_5_to_17 + m_under_5;
-    let remainder_under_5 = 0;
+  let bedrooms = 0;
+  let females = f_5_to_17 + f_under_5;
+  let males = m_5_to_17 + m_under_5;
+  let remainder_under_5 = 0;
 
-    if (females % 2 === 0) {
-        bedrooms += (females / 2);
-    } else if  (f_5_to_17 % 2 === 1) {
-        bedrooms += (((females - 1) / 2) + 1);
-    } else {
-        bedrooms += ((females - 1) / 2);
-        remainder_under_5 += 1;
-    }
+  if (females % 2 === 0) {
+    bedrooms += (females / 2);
+  } else if  (f_5_to_17 % 2 === 1) {
+    bedrooms += (((females - 1) / 2) + 1);
+  } else {
+    bedrooms += ((females - 1) / 2);
+    remainder_under_5 += 1;
+  }
 
-    if (males % 2 === 0) {
-        bedrooms += (males / 2);
-    } else if (m_5_to_17 % 2 === 1) {
-        bedrooms += (((males - 1) / 2) + 1);
-    } else {
-        bedrooms += ((males - 1) / 2);
-        remainder_under_5 += 1;
-    }
+  if (males % 2 === 0) {
+    bedrooms += (males / 2);
+  } else if (m_5_to_17 % 2 === 1) {
+    bedrooms += (((males - 1) / 2) + 1);
+  } else {
+    bedrooms += ((males - 1) / 2);
+    remainder_under_5 += 1;
+  }
 
-    if (remainder_under_5 % 2 === 0) {
-        bedrooms += (remainder_under_5 / 2)
-    } else {
-        bedrooms += (((remainder_under_5 - 1) / 2) + 1);
-    }
+  if (remainder_under_5 % 2 === 0) {
+    bedrooms += (remainder_under_5 / 2)
+  } else {
+    bedrooms += (((remainder_under_5 - 1) / 2) + 1);
+  }
 
-return bedrooms
+  return bedrooms
 }
 
 
@@ -55,21 +55,23 @@ let init = () => {
   let calculate = document.getElementById("rooms");
   calculate.onsubmit = showCalculation;
 
-  let yesChildren = document.getElementById("yesChildren");
-  let noChildren = document.getElementById("noChildren");
-  yesChildren.onclick = showChildren;
-  noChildren.onclick = hideChildren;
+  let children = document.getElementById("children");
+  children.onchange = showChildren;
 };
 
-let showChildren = () => {
-  let container = document.getElementById("children");
-  container.style = "display: block;";
+let showChildren = (_event) => {
+  let container = document.getElementById("container");
+  if (!childrenShown) {
+    container.style = "display: block;";
+  }
+  else{
+    container.style = "display: none;";
+  }
+
+  childrenShown = !childrenShown;
 };
 
-let hideChildren = () => {
-  let container = document.getElementById("children");
-  container.style = "display: none;";
-};
+
 
 let convertToNumber = (value) => {
   let result = parseInt(value, 10);
@@ -88,18 +90,21 @@ function showCalculation(event) {
   const formData = {
     adults: convertToNumber(event.target[0].value),
     spouse: event.target[1].checked,
-    children: event.target[3].checked,
-    f_5_to_17: convertToNumber(event.target[5].value),
-    f_under_5: convertToNumber(event.target[6].value),
-    m_5_to_17: convertToNumber(event.target[7].value),
-    m_under_5: convertToNumber(event.target[8].value)
+    children: event.target[2].checked,
+    f_5_to_17: convertToNumber(event.target[3].value),
+    f_under_5: convertToNumber(event.target[4].value),
+    m_5_to_17: convertToNumber(event.target[5].value),
+    m_under_5: convertToNumber(event.target[6].value)
   };
 
   let output = document.getElementById("output");
-  let total_bedrooms = adult_bedrooms(formData.adults,formData.spouse,formData.children) + children_bedrooms(formData.f_5_to_17, formData.f_under_5, formData.m_5_to_17, formData.m_under_5)
+  let total_bedrooms = adult_bedrooms(formData.adults,formData.spouse,formData.children) + children_bedrooms(formData.f_5_to_17, formData.f_under_5, formData.m_5_to_17, formData.m_under_5);
 
-  output.innerHTML = total_bedrooms
-}
-
-
+    if (total_bedrooms < 1) {
+      output.innerHTML = 'You qualify for a bachelor or a 1 bedroom home.'
+    } else {
+      output.innerHTML = `You qualify for a ${total_bedrooms} bedroom home.`
+    }
+};
+$('.ui.checkbox').checkbox();
 window.onload = init;
